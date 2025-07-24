de915f0fd24a8b26608812e2abe1cbd6c393dc07
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-07-24 09:51:30 -0600

    Creación del ciclo general para el menú

--- main.py
+++ main.py
@@ -35,7 +35,25 @@ def max_min(array):
     minimum = min(array)
     return maximum, minimum
 
-
+while True:
+    print("\n--------Bienvenido al programa----------\n1. obtener sumatoria, promedio y cantidad de positivos y negativos de una serie numérica\n2. calcular el área de un triángulo\n3. verificar si un número es par o impar\n4. calcular promedio de calificaciones\n5. mostrar el mayor y el menor de una serie numérica\n6. salir")
+    option = input("Seleccione una opción: ")
+    match option:
+        case "1":
+            pass
+        case "2":
+            pass
+        case "3":
+            pass
+        case "4":
+            pass
+        case "5":
+            pass
+        case "6":
+            print("Saliendo...")
+            break
+        case _:
+            print("Elija una opción válida")
 
 
 
